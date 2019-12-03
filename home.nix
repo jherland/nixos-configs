6de86784389053625854d02defe607257805ed48
Johan Herland <johan@herland.net>
2019-12-03 01:35:25 +0100

    More setup. Replace vim with neovim/SpaceVim

--- home.nix
+++ home.nix
@@ -16,8 +16,11 @@ in
   home = {
     language.base = "C.UTF-8";
 
-    packages = [
-      pkgs.vivaldi
+    packages = with pkgs; [
+      # fonts
+      powerline-fonts
+
+      vivaldi
     ];
 
     sessionVariables = {
@@ -47,7 +50,7 @@ in
           diff = "git diff --no-index";
           dmesg = "dmesg --human";
           cat = "bat";
-          vi = "vim";
+          # vi = "vim";
       };
     };
 
@@ -134,11 +137,14 @@ in
 
     man.enable = true;
 
+    # Install just enough of neovim to support SpaceVim
     neovim = {
       enable = true;
       viAlias = true;
       vimAlias = true;
       withPython3 = true;
+      plugins = with pkgs.vimPlugins; [
+      ];
     };
 
     ssh = {
@@ -156,34 +162,6 @@ in
       settings = {
         # See https://starship.rs/config for available config directives
         add_newline = false;
-        prompt_order = [
-          "username"
-          "hostname"
-          "kubernetes"
-          "directory"
-          "git_branch"
-          "git_state"
-          "git_status"
-          "package"
-          "dotnet"
-          "golang"
-          "java"
-          "nodejs"
-          "python"
-          "ruby"
-          "rust"
-          "nix_shell"
-          "conda"
-          "aws"
-          "env_var"
-          "cmd_duration"
-          "time"
-          "line_break"
-          "jobs"
-          "battery"
-          "memory_usage"
-          "character"
-        ];
         username.show_always = true;
         username.style_user = "bold green";
         hostname.ssh_only = false;
@@ -222,10 +200,97 @@ in
       # terminal = "screen-256color";
     };
 
-#     vim = {
-#       enable = true;
-#       plugins = [ pkgs.vimPlugins.spacevim ];
-#     };
+    # Nix, Y U NO HAZ SpaceVim!
+    vim = {
+      enable = false;
+      plugins = with pkgs.vimPlugins; [
+        syntastic
+        gruvbox-community
+      ];
+      extraConfig = ''
+        " Use vim settings, rather then vi settings (much better!)
+        " This must be first, because it changes other options as a side effect.
+        set nocompatible
+
+        set t_Co=256            " terminal supports 256 color mode.
+        set ai                  " auto indenting
+        set history=100         " keep 100 lines of history
+        set ruler               " show the cursor position
+        syntax on               " syntax highlighting
+        filetype plugin on      " use the file type plugins
+
+        set showmode                    " always show what mode we're currently editing in
+
+        set tabstop=4                   " a tab is four spaces
+        set softtabstop=4               " when hitting <BS>, pretend like a tab is removed, even if spaces
+        set expandtab                   " expand tabs to spaces by default
+        set shiftwidth=4                " number of spaces to use for autoindenting
+        set shiftround                  " use multiple of shiftwidth when indenting with '<' and '>'
+        set backspace=indent,eol,start  " allow backspacing over everything in insert mode
+        set autoindent                  " always set autoindenting on
+        set copyindent                  " copy the previous indentation on autoindenting
+
+        set showmatch                   " set show matching parenthesis
+        set smarttab                    " insert tabs on the start of a line according to
+                                        "    shiftwidth, not tabstop
+        set scrolloff=4                 " keep 4 lines off the edges of the screen when scrolling
+
+        set hlsearch                    " highlight search terms
+        set incsearch                   " show search matches as you type
+
+        set mouse=a                     " enable using the mouse if terminal emulator supports it
+        " Consider turning it off if it kills copy/paste
+
+        " Editor layout {{{
+        set termencoding=utf-8
+        set encoding=utf-8
+        set lazyredraw                  " don't update the display while executing macros
+        set laststatus=2                " tell VIM to always put a status line in, even
+                                        "    if there is only one window
+        set cmdheight=1                 " use a status bar that is 1 rows high
+
+        " Vim behaviour {{{
+        set history=1000                " remember more commands and search history
+        set undolevels=1000             " use many muchos levels of undo
+        set undofile                    " keep a persistent backup file
+        set undodir=~/.vim/.undo,~/tmp,/tmp
+        set nobackup                    " do not keep backup files, it's 70's style cluttering
+        set noswapfile                  " do not write annoying intermediate swap files,
+                                        "    who did ever restore from swap files anyway?
+        set directory=~/.vim/.tmp,~/tmp,/tmp
+                                        " store swap files in one of these directories
+                                        "    (in case swapfile is ever turned on)
+        set viminfo='20,\"80            " read/write a .viminfo file, don't store more
+                                        "    than 80 lines of registers
+        set wildmenu                    " make tab completion for files/buffers act like bash
+        set wildmode=list:full          " show a list when pressing tab and complete
+                                        "    first full match
+        set wildignore=*.swp,*.bak,*.pyc,*.class
+        set title                       " change the terminal's title
+        set visualbell                  " don't beep
+        set noerrorbells                " don't beep
+        set showcmd                     " show (partial) command in the last line of the screen
+                                        "    this also shows visual selection info
+        set nomodeline                  " disable mode lines (security measure)
+
+        set hidden
+
+        " Colors {{{
+        set background=dark
+        colorscheme gruvbox
+        let g:gruvbox_contrast_dark = 'hard'
+
+        " Syntastic {{{
+        " set statusline+=%#warningmsg#
+        " set statusline+=%{SyntasticStatuslineFlag()}
+        " set statusline+=%*
+
+        let g:syntastic_always_populate_loc_list = 1
+        let g:syntastic_auto_loc_list = 1
+        let g:syntastic_check_on_open = 1
+        let g:syntastic_check_on_wq = 0
+      '';
+    };
   };
 
   services.gpg-agent = {
