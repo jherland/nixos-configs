04f91541a0f2093f6d0985e56d06ca1c3611ca9d
Johan Herland <johan@herland.net>
2020-10-18 19:25:16 +0200

    kill-ssh-control-masters.sh: Fix script, -S is useless without knowing the destination

--- kill-ssh-control-masters.sh
+++ kill-ssh-control-masters.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Assume SSH ControlMaster sockets are named according to home-manager's
 # default ControlPath directive: `ControlPath ~/.ssh/master-%r@%n:%p`
 
 for sockpath in ~/.ssh/master-*; do
     echo "$sockpath"
-    ssh -O exit -S "$sockpath"
+    port=${sockpath#*:}
+    dst=${sockpath#*/master-}
+    dst=${dst%:$port}
+    ssh -O exit "$dst" -p "$port"
 done
