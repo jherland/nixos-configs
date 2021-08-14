ec8c7ae2a2726701b81c9033dcd4c996e5de0c91
Johan Herland <johan@herland.net>
2021-08-14 17:27:47 +0200

    gui_work: Pull Webex client from myme's PR rather than a work repo
    This enables running `home-manager switch` without being on the work
    VPN.

--- gui_work.nix
+++ gui_work.nix
@@ -1,6 +1,13 @@
 { pkgs, ...}:
 
-{
+let
+  webexPkgs = import (pkgs.fetchFromGitHub {
+    owner = "myme";
+    repo = "nixpkgs";
+    rev = "29d6b2257a6eeb43dc6b8b7c1576288825250a11";
+    sha256 = "0wd57ryhsc90kvv0qbrd5l96v1ac4s18yzpc6bn3vppx76xfxj5z";
+  }) {};
+in {
   nixpkgs.overlays = [
     (import "${builtins.fetchTarball https://github.com/vlaci/openconnect-sso/archive/master.tar.gz}/overlay.nix")
   ];
@@ -16,7 +23,7 @@
       openconnect-sso
 
       # Webex
-      (callPackage ./webex-linux-nix {})
+      webexPkgs.webex
     ];
   };
 
