4ff4bc9581de828c53b66f12acd9771a5ddd9a66
Johan Herland <johan@herland.net>
2022-11-04 21:03:04 +0100

    rebuild-system.sh: Be smarter about comparing symlinks and remote deploys
    Instead of forwarding command-line arguments to nixos-rebuild, always
    perform a fully local "nixos-rebuild ... build", and print the command
    needed to deploy. The local build is needed in order for the ./result
    symlink to be created.
    
    Also we don't want to build with --target-host as that copies the closure
    to the remote system without enabling it, as wasteful copy in case we
    don't actually do the switch.
    
    When run with no hostname, default to the local host.
    
    Finally, keep separate symlinks per-host, so that the old/new comparison
    will show relevant symlinks (instead of comparing apples to pears).

--- rebuild-system.sh
+++ rebuild-system.sh
@@ -3,16 +3,38 @@
 set -e
 
 host=$1
-shift
-
-if [ -h ./result ]; then
-  rm -f ./old-result
-  ln -s "$(readlink ./result)" ./old-result
+if [ -z "$host" ]; then
+    host=$(hostname)
 fi
 
-nixos-rebuild --flake "./systems/${host}#${host}" --target-host "root@${host}" $@
+new="./result.${host}.new"
+old="./result.${host}.old"
 
-if [ -h old-result ]; then
-  echo "old result: $(readlink old-result)"
+copy_symlink() {
+    src=$1
+    dst=$2
+    if [ -h "$src" ]; then
+        rm -f "$dst"
+        ln -s "$(readlink "$src")" "$dst"
+    else
+        echo "${src} is not a symlink!"
+        false
+    fi
+}
+
+nixos-rebuild --flake "./systems/${host}#${host}" build
+copy_symlink "$new" "$old" || true
+copy_symlink ./result "$new"
+
+if [ -h "$old" ]; then
+    echo "old result: $(readlink "$old")"
 fi
-echo "new result: $(readlink result)"
+echo "new result: $(readlink "$new")"
+
+if [ "${host}" = "$(hostname)" ]; then
+    preamble="sudo "
+else
+    extra_args=" --target-host \"root@${host}\""
+fi
+echo "To deploy:"
+echo "    ${preamble}nixos-rebuild --flake \"./systems/${host}#${host}\"${extra_args} switch"
