89f6ec7c6459e6dc8a729f527330cfeaef045582
Johan Herland <johan@herland.net>
2022-11-16 17:31:07 +0100

    Move common config into common/base.nix

---
+++ common/base.nix
@@ -0,0 +1,24 @@
+{ self, hostname, nixpkgs, ...}:
+{
+  boot.loader.timeout = 1;
+  networking.hostName = hostname;
+
+  nix = {
+    registry.nixpkgs.flake = nixpkgs;
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  system = {
+    configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
+
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    stateVersion = "22.05"; # Did you read the comment?
+  };
+}
--- flake.nix
+++ flake.nix
@@ -4,56 +4,37 @@
     nixos-hardware.url = github:NixOS/nixos-hardware;
   };
 
-
-  outputs = { self, ... }@inputs:
-  let
-    common-system-config = (hostname: {
-      boot.loader.timeout = 1;
-      networking.hostName = hostname;
-
-      nix = {
-        registry.nixpkgs.flake = inputs.nixpkgs;
-        settings.experimental-features = [ "nix-command" "flakes" ];
-      };
-
-      nixpkgs.config.allowUnfree = true;
-
-      system = {
-        configurationRevision = inputs.nixpkgs.lib.mkIf (self ? rev) self.rev;
-
-        # This value determines the NixOS release from which the default
-        # settings for stateful data, like file locations and database versions
-        # on your system were taken. It‘s perfectly fine and recommended to leave
-        # this value at the release version of the first install of this system.
-        # Before changing this value read the documentation for this option
-        # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-        stateVersion = "22.05"; # Did you read the comment?
-      };
-    });
-  in {
+  outputs = inputs:
+  {
     nixosConfigurations.beta = inputs.nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        specialArgs = inputs // {self = self; };
+        specialArgs = inputs // {
+	  hostname = "beta";
+	};
         modules = [
-          (common-system-config "beta")
+	  ./common/base.nix
           ./systems/beta/configuration.nix
         ];
     };
 
     nixosConfigurations.chi = inputs.nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        specialArgs = inputs // {self = self; };
+        specialArgs = inputs // {
+	  hostname = "chi";
+	};
         modules = [
-          (common-system-config "chi")
+	  ./common/base.nix
           ./systems/chi/configuration.nix
         ];
     };
 
     nixosConfigurations.delta = inputs.nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        specialArgs = inputs // {self = self; };
+        specialArgs = inputs // {
+	  hostname = "delta";
+	};
         modules = [
-          (common-system-config "delta")
+	  ./common/base.nix
           ./systems/delta/configuration.nix
         ];
     };
