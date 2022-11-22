e47997659463427c5852b2c9611daf93c80c45d2
Johan Herland <johan@herland.net>
2022-11-22 23:51:54 +0100

    epsilon: First attempt to bring configuration.nix up to date
    Leave custom stuff commented in case we need to re-enable.

--- systems/epsilon/configuration.nix
+++ systems/epsilon/configuration.nix
@@ -1,147 +1,89 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
   imports = [
     # Hardware-specific
+    # MISSING nixos-hardware module for ASUS MiniPC PN51-E1
     ./hardware-configuration.nix
+
+    # Common subsets
+    ../../common/base.nix
+    ../../common/hw_logitech.nix
+    ../../common/i18n_en_nl.nix
+    ../../common/laptop.nix  # ???
+    ../../common/user_jherland.nix
+    ../../common/gui_jherland.nix
   ];
 
-  # Accelerated VMs
-  boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.consoleMode = "auto";
+  boot.loader.systemd-boot.memtest86.enable = true;
 
   # Try to avoid amdgpu crashes by upgrading kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+#  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # Use cgroups v2 unified hierarchy only
-  boot.kernelParams = [ "systemd.unified_cgroup_hierarchy=1" ];
+#  boot.kernelParams = [ "systemd.unified_cgroup_hierarchy=1" ];
 
   # Add filesystems possibly needed after boot
-  boot.supportedFilesystems = [ "nfs" "exfat" ];
+#  boot.supportedFilesystems = [ "nfs" "exfat" ];
 
-  nixpkgs.config.allowUnfree = true;
+  # Make console font a little larger and more legible
+  console = {
+    font = "ter-v20n";
+    packages = with pkgs; [ terminus_font ];
+  };
 
-  networking.hostName = "epsilon"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Set your time zone.
-  time.timeZone = "Europe/Oslo";
+  # Disable NetworkManager from common/laptop.nix
+#  networking.networkmanager.enable = false;
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp2s0.useDHCP = true;
-  networking.interfaces.wlp3s0.useDHCP = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  i18n = {
-    defaultLocale = "C.UTF-8";
-    supportedLocales = [ "C.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" "nb_NO.UTF-8/UTF-8" ];
-  };
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "us";
-  };
+#  networking.useDHCP = false;
+#  networking.interfaces.enp2s0.useDHCP = true;
+#  networking.interfaces.wlp3s0.useDHCP = true;
 
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+#  services.printing.enable = true;
 
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-  # hardware.pulseaudio.package = pkgs.pulseaudio.override { jackaudioSupport = true; };
+#   users.users.jherland = {
+#     extraGroups = [
+# #      "audio"
+# #      "docker"
+# #      "jackaudio"
+# #      "networkmanager"
+#       "plugdev"
+#       "wheel"
+#     ];
+#     uid = 1000;
+#     openssh.authorizedKeys.keys = [
+#       "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLJTVKcMaGbbkG1t2+GjMN1YeZL7DdQXLPxO5MlHVGXhHbXQfrxAPTM9ArxtiRNEeYvuFAxxzSlcA1kl37/S4y0= jherland@alpha"
+#       "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBG6ntd6Sqhdjnf0gCV0Urwvxw5O9WOq63plMRyNnHrr9/YW0aPpbgXbl+/jNYZoFjb0fg7/bGhDnVi+R7/et3OY= jherland@beta"
+#       "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBPUtBTKag3FMgq68JEDtVNTBBxQJzhSJHgm9zA4OhUy5wcRBmKpj10iprq5msbN5dwLtJYRF0D4KRMZthpDcXTw= u0_a381@localhost"
+#       "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBBRhfBOYn7bny7f4gChbS71MOwASBnggRbUMeL6izzgMkL5Zd3IsouuRvL6HyRczpV+V6LHzIw6Y+hbYsERh7eI= u0_a37@localhost"
+#     ];
+#   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.jane = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  # };
-
-  users.users.jherland = {
-    isNormalUser = true;
-    description = "Johan Herland";
-    extraGroups = [
-#      "audio"
-#      "docker"
-#      "jackaudio"
-#      "networkmanager"
-      "plugdev"
-      "wheel"
-    ];
-    uid = 1000;
-    openssh.authorizedKeys.keys = [
-      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLJTVKcMaGbbkG1t2+GjMN1YeZL7DdQXLPxO5MlHVGXhHbXQfrxAPTM9ArxtiRNEeYvuFAxxzSlcA1kl37/S4y0= jherland@alpha"
-      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBG6ntd6Sqhdjnf0gCV0Urwvxw5O9WOq63plMRyNnHrr9/YW0aPpbgXbl+/jNYZoFjb0fg7/bGhDnVi+R7/et3OY= jherland@beta"
-      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBPUtBTKag3FMgq68JEDtVNTBBxQJzhSJHgm9zA4OhUy5wcRBmKpj10iprq5msbN5dwLtJYRF0D4KRMZthpDcXTw= u0_a381@localhost"
-      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBBRhfBOYn7bny7f4gChbS71MOwASBnggRbUMeL6izzgMkL5Zd3IsouuRvL6HyRczpV+V6LHzIw6Y+hbYsERh7eI= u0_a37@localhost"
-    ];
-  };
-
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  #   firefox
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
+  # Enable hosting containers
   virtualisation.podman = {
     enable = true;
     dockerCompat = true;
   };
 
-  # Enable VMs
+  # Enable hosting VMs
   virtualisation.libvirtd.enable = true;
 
-  # List services that you want to enable:
-
-  # F/W update daemon
-  services.fwupd.enable = true;
-
   # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    passwordAuthentication = false;
-  };
-
-  services.tailscale = {
-    enable = true;
-#    package = unstable.tailscale;
-  };
-
-  # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
-    layout = "us(intl-unicode),no(nodeadkeys)";
-    xkbOptions = "eurosign:e,caps:ctrl_modifier";
-
-    # Enable the Plasma 5 Desktop Environment.
-    displayManager.sddm.enable = true;
-    desktopManager.plasma5.enable = true;
-  };
+# services.openssh = {
+#   enable = true;
+#   passwordAuthentication = false;
+# };
 
   # Setup YubiKey support
-  services.udev.packages = [ pkgs.yubikey-personalization ];
-  services.pcscd.enable = true;
+#  services.udev.packages = [ pkgs.yubikey-personalization ];
+#  services.pcscd.enable = true;
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [
@@ -157,15 +99,14 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.05"; # Did you read the comment?
+#  system.stateVersion = "21.05"; # Did you read the comment?
 
-  system.autoUpgrade = {
-    enable = true;
-    allowReboot = false;
-    channel = "https://nixos.org/channels/nixos-21.05";
-  };
+# system.autoUpgrade = {
+#   enable = true;
+#   allowReboot = false;
+#   channel = "https://nixos.org/channels/nixos-21.05";
+# };
 
-  nix.gc.automatic = true;
-  nix.gc.dates = "03:15";
+# nix.gc.automatic = true;
+# nix.gc.dates = "03:15";
 }
-
