1fa468a67f28c2ba138e868ceefcb93bb8734620
Johan Herland <johan@herland.net>
2023-10-28 13:45:47 +0200

    chi: Import home-manager config into system config

--- flake.nix
+++ flake.nix
@@ -24,7 +24,14 @@
       chi = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = inputs // { hostname = "chi"; };
-        modules = [ ./chi/configuration.nix ];
+        modules = [
+          ./chi/configuration.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.jherland = import ./chi/home.nix;
+          }
+        ];
       };
 
       delta = nixpkgs.lib.nixosSystem {
